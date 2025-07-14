028ae0715e26e243c18eb84717d22b5919c3b2d9
asimwe1 <landryasimwe@gmail.com>
2025-07-14 15:30:00 +0200

    Update reward pool initialization with new security checks

--- contracts/RewardPoolContract.sol
+++ contracts/RewardPoolContract.sol
@@ -3,34 +3,48 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// Manages reward pool for GreenMint App challenges
 contract RewardPoolContract is AccessControl {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     IERC721 public nftContract;
+    IERC20 public tokenContract;
+    address public backendAddress;
 
-    // Mapping to store challenge rewards (NFT token IDs)
     mapping(uint256 => uint256[]) public challengeRewards;
+    mapping(uint256 => uint256[]) public marketplaceRewards;
 
-    constructor(address _nftContract) {
+    constructor(address _nftContract, address _tokenContract, address _backendAddress) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(ADMIN_ROLE, msg.sender);
         nftContract = IERC721(_nftContract);
+        tokenContract = IERC20(_tokenContract);
+        backendAddress = _backendAddress;
     }
 
-    // Deposits NFTs into the reward pool
-    function depositNFTs(uint256 _challengeId, uint256[] memory _tokenIds) external onlyRole(ADMIN_ROLE) {
+    function depositNFTs(uint256 _id, uint256[] memory _tokenIds, bool _isMarketplace) external onlyRole(ADMIN_ROLE) {
         for (uint256 i = 0; i < _tokenIds.length; i++) {
             nftContract.transferFrom(msg.sender, address(this), _tokenIds[i]);
-            challengeRewards[_challengeId].push(_tokenIds[i]);
+            if (_isMarketplace) {
+                marketplaceRewards[_id].push(_tokenIds[i]);
+            } else {
+                challengeRewards[_id].push(_tokenIds[i]);
+            }
         }
     }
 
-    // Distributes an NFT to a challenge winner
-    function distributeReward(uint256 _challengeId, address _recipient) external onlyRole(ADMIN_ROLE) {
-        require(challengeRewards[_challengeId].length > 0, "No rewards available");
-        uint256 tokenId = challengeRewards[_challengeId][challengeRewards[_challengeId].length - 1];
-        challengeRewards[_challengeId].pop();
+    function distributeReward(uint256 _id, address _recipient, bool _isMarketplace) external {
+        require(msg.sender == backendAddress, "Only backend can distribute");
+        uint256[] storage rewards = _isMarketplace ? marketplaceRewards[_id] : challengeRewards[_id];
+        require(rewards.length > 0, "No rewards available");
+        uint256 tokenId = rewards[rewards.length - 1];
+        rewards.pop();
         nftContract.transferFrom(address(this), _recipient, tokenId);
+    }
+
+    function distributeTokenReward(address _recipient, uint256 _amount) external {
+        require(msg.sender == backendAddress, "Only backend can distribute");
+        require(tokenContract.balanceOf(address(this)) >= _amount, "Insufficient token balance");
+        tokenContract.transfer(_recipient, _amount);
     }
 }
