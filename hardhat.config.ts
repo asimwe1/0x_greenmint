ca35f88004c8af0af69d93ce35d06732437e041d
asimwe1 <landryasimwe@gmail.com>
2025-07-15 18:45:00 +0200

    Enable optimizer settings for production deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -9,6 +11,13 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 200
       }
+    },
+  },
+  networks: {
+    hardhat: {},
+    palm: {
+      url: process.env.PALM_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY!]
     }
   },
   paths: {
